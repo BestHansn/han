15733c444330d59cd47508dc9b72728764d58ba3
hansn007 <31610900+hansn007@users.noreply.github.com>
2017-09-11 14:20:54 +0800

    hahaha

---
+++ Test.java
@@ -0,0 +1,17 @@
+package p1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Rectangle rectangle = new Rectangle(3,5);
+		
+		rectangle.perimeter();
+		rectangle.area();
+		
+		
+	}
+	
+	
+	
+}
